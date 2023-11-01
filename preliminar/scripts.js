01ed411382ee97705e6b877503dbfe461a853d6b
HeladeraDr <dantelin2009@gmail.com>
2023-11-01 19:11:24 -0300

    Add files via upload

---
+++ preliminar/scripts.js
@@ -0,0 +1,53 @@
+function openLink(link){
+    window.open(link, "_blank")
+}
+
+function scrollToContent(){
+    let element = document.getElementById("content");
+    element.scrollIntoView({behavior: 'smooth'});
+}
+
+function cScroll(element){
+    end = element.scrollLeft + element.clientWidth >= element.scrollWidth - 50;
+    if(end){
+        element.scroll({
+            top: 0,
+            left: 0,
+            behavior: "smooth"
+        });
+        buttonLabel = "keyboard_arrow_right";
+        return;
+    }
+    element.scrollBy({
+        top: 0,
+        left: 272,
+        behavior: "smooth"
+    });
+    setTimeout(() => {
+        cScrollCheck(element);
+    }, 300)
+}
+
+function cScrollCheck(element){
+    let buttonLabel = element.parentElement.querySelector(".material-symbols-outlined");
+    end = element.scrollLeft + element.clientWidth >= element.scrollWidth - 50;
+    if(end){
+        buttonLabel.innerHTML = "keyboard_arrow_left";
+    }
+    else{
+        buttonLabel.innerHTML = "keyboard_arrow_right";
+    }
+}
+
+document.querySelectorAll(".carrousel").forEach((element) => {
+    element.addEventListener("scroll", () => cScrollCheck(element))
+})
+
+window.addEventListener("scroll", () => {
+  if(window.scrollY > (window.innerHeight - 364)){
+      document.querySelector(".logos").classList.add("absolute");
+  }
+  else{
+      document.querySelector(".logos").classList.remove("absolute");
+  }
+});
